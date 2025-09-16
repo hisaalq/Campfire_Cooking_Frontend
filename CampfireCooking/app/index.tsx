8f4b0f494274f54cf66e43f86c898334e7f0342d
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-09-16 16:22:56 +0300

    main

--- CampfireCooking/app/index.tsx
+++ CampfireCooking/app/index.tsx
@@ -4,22 +4,6 @@ import React from "react";
 
 const Index = () => {
   return <LoginScreen />;
-import { Stack } from "expo-router";
-import CategoriesSection from "@/components/categories";
-import React from "react";
-
-const Index = () => {
-  return (
-    <>
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-      />
-      <CategoriesSection />
-    </>
-  );
-
 };
 
 export default Index;
